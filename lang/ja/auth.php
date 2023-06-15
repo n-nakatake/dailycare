c3e3c8f059355c16ba5b6739d824db08e14687bb
n-nakatake <nakatake@iroiro-inc.com>
2023-06-15 10:07:45 +0900

    ログイン時のエラー文言を修正

--- lang/ja/auth.php
+++ lang/ja/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'ユーザーIDまたはパスワードが正しくありません',
+    'password' => 'ユーザーIDまたはパスワードが正しくありません',
+    'throttle' => '連続してログインに失敗した時間を置いてから再度ログインして下さい。',
 
 ];
