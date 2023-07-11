eb39a1baf284b485504e9e03cfaf3394b9fe35b8
n-nakatake <nakatake@iroiro-inc.com>
2023-07-11 11:36:15 +0900

    LaravelMixでコンパイルされないように設定を変更

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/admin.scss', 'public/css')
-    .sass('resources/sass/profile.scss', 'public/css')
-    .sourceMaps();
+// LaravelMixは当面使わないのでコメントアウトしておく
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css')
+//     .sass('resources/sass/admin.scss', 'public/css')
+//     .sass('resources/sass/profile.scss', 'public/css')
+//     .sourceMaps();
